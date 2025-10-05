3993a64e3b6a772a0e609e2ec4bc7aae6f7fb6bc
beq <erlepesbek@gmail.com>
2025-10-05 14:55:39 +0500

    video

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -349,9 +349,10 @@ const LandingPage = () => {
             minHeight: '100%'
           }}
         >
-          {/* Using external video sources */}
+          {/* Using local NASA video */}
+          <source src="/video.mp4" type="video/mp4" />
+          {/* Fallback to external sources if local video fails */}
           <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
-          <source src="https://sample-videos.com/zip/10/mp4/SampleVideo_1280x720_1mb.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </BackgroundVideo>
         <VideoOverlay />
